09b9dd85af6466cbfbd0301b77b9d6352869b886
ashutoshjha23 <jashutosh498@gmail.com>
2023-04-10 16:17:36 +0530

    Simplify the BoilerPlate

---
+++ project1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hello, This is AJ
+      </header>
+    </div>
+  );
+}
+
+export default App;
